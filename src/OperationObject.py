05b7f4d9eba6faade2a6e641824211daad22075c
miracleave <66664167+miracleave-ltd@users.noreply.github.com>
2021-09-17 21:03:50 +0900

    Update OperationObject.py
    コメント修正

--- src/OperationObject.py
+++ src/OperationObject.py
@@ -7,14 +7,14 @@ GOOGLE_APPLICATION_CREDENTIALS='./credential/key.json'
 source_file_name = './csv/gcs-example.csv'
 
 # CSV配置先
-url_gs_example_csv="gs://mira-example/gcs-example.csv" # 要変更1.1
+url_gs_example_csv="gs://mira-example/gcs-example.csv" # 手順1.1
 # CSVダウンロード先
-out_url_gs_example_csv="gs://mira-example/out-gcs-example.csv" # 要変更1.1
+out_url_gs_example_csv="gs://mira-example/out-gcs-example.csv" # 手順1.1
 
 # GCSバケット
-bucket_name = "mira-example" # 要変更1.2
+bucket_name = "mira-example" # 手順1.2
 
 # 操作テーブル
-project_id = "erudite-pride-323410" # 要変更1.3
-dataset_id = "mira_vol24" # 要変更1.4
-table_id = "mira_example" # 要変更1.5
+project_id = "erudite-pride-323410" # 手順1.3
+dataset_id = "mira_vol24" # 手順1.4
+table_id = "mira_example" # 手順1.5
