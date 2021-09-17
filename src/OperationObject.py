125f1aaa7fe7792691253dc2bc350fc596c00103
miracleave <66664167+miracleave-ltd@users.noreply.github.com>
2021-09-17 10:12:03 +0900

    Update OperationObject.py
    コメント修正

--- src/OperationObject.py
+++ src/OperationObject.py
@@ -6,15 +6,15 @@ GOOGLE_APPLICATION_CREDENTIALS='./credential/key.json'
 # CSVコピー元
 source_file_name = './csv/gcs-example.csv'
 
-# GS バケット（作成したグローバルユニークなGSバケットに変更してください）
-bucket_name = "mira-example"
+# CSV配置先
+url_gs_example_csv="gs://mira-example/gcs-example.csv" # 要変更1.1
+# CSVダウンロード先
+out_url_gs_example_csv="gs://mira-example/out-gcs-example.csv" # 要変更1.1
 
-# CSV配置先（作成したグローバルユニークなGSバケットに変更してください）
-url_gs_example_csv="gs://mira-example/gcs-example.csv"
-# CSVダウンロード先（作成したグローバルユニークなGSバケットに変更してください）
-out_url_gs_example_csv="gs://mira-example/out-gcs-example.csv"
+# GCSバケット
+bucket_name = "mira-example" # 要変更1.2
 
 # 操作テーブル
-project_id = "erudite-pride-323410" # （使用するプロジェクトＩＤに変更してください）
-dataset_id = "mira_vol24"
-table_id = "mira_example"
+project_id = "erudite-pride-323410" # 要変更1.3
+dataset_id = "mira_vol24" # 要変更1.4
+table_id = "mira_example" # 要変更1.5
