bd5217068c47798be689122bcf7116711fe582b5
miracleave <66664167+miracleave-ltd@users.noreply.github.com>
2021-09-23 20:28:23 +0900

    Update ExportBigQuery04.py

--- src/ExportBigQuery04.py
+++ src/ExportBigQuery04.py
@@ -2,19 +2,19 @@ import os
 import OperationObject # 操作対象の設定情報取得
 from google.cloud import bigquery
 
-# GCS認証設定
+# GCP認証設定
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = OperationObject.GOOGLE_APPLICATION_CREDENTIALS
 
 # BigQueryクライアントAPIの利用宣言
 client = bigquery.Client(OperationObject.project_id)
-# テーブルIDの取得
-table_id = client.dataset(OperationObject.dataset_id).table(OperationObject.table_id)
+# テーブル情報の取得
+table_info = client.dataset(OperationObject.dataset_id).table(OperationObject.table_id)
 
 # データ取得結果をGCSバケットにエキスポート 
 extract_job = client.extract_table(
-    table_id,
+    table_info,
     OperationObject.out_url_gs_example_csv,
-)  # クライアントAPIへリクエスト
+)
 
 extract_job.result() # extract_tableが終了するまで待機
 
